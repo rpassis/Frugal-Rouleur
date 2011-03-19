01af40bf6b94ee402b0d7a34b5d92cc6ab97c9ac
jimwhimpey <jimwhimpey@me.com>
2011-03-19 22:27:36 +1000

    Switched the parsing to individual sites which return JSON. Chain reaction is now working

--- rouleur.rb
+++ rouleur.rb
@@ -6,6 +6,7 @@ require 'curb'
 require 'nokogiri'
 require 'maruku'
 require 'open-uri'
+require 'json'
 
 configure :development do
   require "sinatra/reloader"
@@ -13,6 +14,10 @@ end
 
 # Default is xhtml, do not want!
 set :haml, {:format => :html5, :escape_html => false}
+
+
+
+
 
 # Homepage
 get '/' do
@@ -26,59 +31,75 @@ get '/search/:term' do
 	# The search term
 	search = params[:term];
 	
-	# Search URLs
-	#sites = Hash[	"Chain Reaction" => "http://www.chainreactioncycles.com/SearchResults.aspx?Search=",
-	#							"Wiggle" => "http://www.wiggle.co.uk/?s=",
-	#							"ProBikeKit" => "http://www.probikekit.com/advsearch.php?AQUERY=",
-	#							"Ribble Cycles" => "http://www.ribblecycles.co.uk/product/t/"]
-	
-	sites = Hash[	"Chain Reaction" => "http://www.chainreactioncycles.com/SearchResults.aspx?Search="]
-
-	# New hash for holding the results
-	results = Hash.new
-
-	# Loop through each of the sites
-	sites.each do |site, url| 
-		
-		# Parse the doc into Nokogiri
-		doc = Nokogiri::HTML(open(url + search))
-		
-		# Different rules for parsing the returned doc depending on the site
-		if site == "Chain Reaction" then
-			
-			# Create hashes for the amount of results we want returned
-			results["Chain Reaction"] = Array.new
-			for i in 1..3 do
-				results["Chain Reaction"][i] = Hash.new
-				# Get the name, price, URL and image URL
-				results["Chain Reaction"][i]["name"] 	= doc.css("#ModelLink#{i}")[0].attribute("title").value
-				results["Chain Reaction"][i]["price"] 	= doc.css("#ModelPrice#{i} .Label11")[0].content.gsub(/Now\302\240\302\243/, '')
-				results["Chain Reaction"][i]["url"] 		= "http://chainreactioncycles.com" + doc.css("#ModelLink#{i}")[0].attribute("href").value
-				results["Chain Reaction"][i]["image"] 	= "http://chainreactioncycles.com" + doc.css("#ModelImageLink#{i} img")[0].attribute("src").value
-			end
-			
-		elsif site == "Wiggle" then
-			
-		elsif site == "ProBikeKit" then
-			
-		elsif site == "Ribble Cycles" then
-			
-		end
-		
-	end
-
-	puts results.inspect
-	
   # Render the HAML template
   haml :home
 
 end
 
+
+
+
+
+
+
 # Takes in arguments for site, search term and number of results to return and returns JSON
 # This will be called by JavaScript so that we can progressively show each site's results
 get '/json/:site/:term/:number' do
 	
+	# We'll be returning JSON
+	content_type 'application/javascript'
+	
+	# New JSON object for holding the results
+	results = "["
+	
+	
+	
+	# Different rules for parsing the returned doc depending on the site
+	if params[:site] == "Chain Reaction" then
+		
+		# Parse the doc into Nokogiri
+		doc = Nokogiri::HTML(open("http://www.chainreactioncycles.com/SearchResults.aspx?Search=" + params[:term]))
+		
+		# Loop through the number of items we want returned creating a little JSON object for each
+		for i in 1..params[:number].to_i do
+			results += "{"
+			results += "\"name\": \"" + doc.css("#ModelLink#{i}")[0].attribute("title").value + "\","
+			results += "\"price\": \"" + doc.css("#ModelPrice#{i} .Label11")[0].content.gsub(/Now\302\240\302\243/, '') + "\","
+			results += "\"url\": \"" + "http://chainreactioncycles.com" + doc.css("#ModelLink#{i}")[0].attribute("href").value + "\","
+			results += "\"image\": \"" + "http://chainreactioncycles.com" + doc.css("#ModelImageLink#{i} img")[0].attribute("src").value + "\""
+			results += "},"
+		end
+		
+	elsif params[:site] == "Wiggle" then
+		
+		# Parse the doc into Nokogiri
+		doc = Nokogiri::HTML(open("http://www.wiggle.co.uk/?s=" + params[:term]))
+		
+	elsif params[:site] == "ProBikeKit" then
+		
+		# Parse the doc into Nokogiri
+		doc = Nokogiri::HTML(open("http://www.probikekit.com/advsearch.php?AQUERY=" + params[:term]))
+		
+	elsif params[:site] == "Ribble+Cycles" then
+		
+		# Parse the doc into Nokogiri
+		doc = Nokogiri::HTML(open("http://www.ribblecycles.co.uk/product/t/" + params[:term]))
+		
+	end
+		
+		
+		
+	
+	# Chop off the last character and close the JSON
+	return results.chop! << "]"
+	
 end
+
+
+
+
+
+
 
 # Conversion of the SCSS to regular CSS
 get '/style.css' do
