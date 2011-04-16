78d0ade82dab87098f5532b35e4d435851dc148d
jimwhimpey <jimwhimpey@me.com>
2011-04-16 18:37:37 +1000

    Added message for no results

--- public/population.js
+++ public/population.js
@@ -18,21 +18,31 @@ $(function(){
 		// Make the AJAX call to grab the results
 		$.get("/json/" + store + "/" + search + "/2", function(data){
 			
-			// Start creating the holder
-			var html = "<table>";
+			// If there's any results at all
+			if (data.Results != "None") {
+				
+				// Start creating the holder
+				var html = "<table>";
 			
-			// Loop through the results and form the HTML row
-			for (var i=0; i < data.Results.length; i++) {
-				html += "<tr>";
-				html += "<td><a href='" + data.Results[i].url + "'><img src='" + data.Results[i].image + "' alt='" + data.Results[i].name + "' /></a></td>";
-				html += "<td><a href='" + data.Results[i].url + "'>" + data.Results[i].name + "</a></td>";
-				html += "<td>AUD" + data.Results[i].price + "</td>";
-				html += "</tr>";
-			};
+				// Loop through the results and form the HTML row
+				for (var i=0; i < data.Results.length; i++) {
+					html += "<tr>";
+					html += "<td><a href='" + data.Results[i].url + "'><img src='" + data.Results[i].image + "' alt='" + data.Results[i].name + "' /></a></td>";
+					html += "<td><a href='" + data.Results[i].url + "'>" + data.Results[i].name + "</a></td>";
+					html += "<td>AUD" + data.Results[i].price + "</td>";
+					html += "</tr>";
+				};
 			
-			// Close up the HTML and append it to the page
-			html += "</table>";
-			result.append(html);
+				// Close up the HTML and append it to the page
+				html += "</table>";
+				result.append(html);
+				
+			} else {
+				
+				// No results message
+				result.append("<p class='noresults'>No Results");
+				
+			}
 			
 		}, "json");
 		
